435e709ec7cf5a373038a2dcfe847f0e5a8d0d31
Nilay Doshi <59107722+niayniay99@users.noreply.github.com>
2022-09-07 22:03:24 +0530

    Add files via upload

---
+++ scan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class scan
+{
+	public static void main(String[] args)
+	{
+	Scanner input = new Scanner(System.in);
+	System.out.println("Welcome!");
+	System.out.println("What is your name ?");
+	String name = input.next();
+	System.out.println("Hello! "+name);
+	
+
+	}
+}
