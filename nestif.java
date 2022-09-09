66a29b9807fb0a87872b5f2025deea8f89ca06e8
Nilay Doshi <59107722+niayniay99@users.noreply.github.com>
2022-09-09 21:12:51 +0530

    Add files via upload

---
+++ nestif.java
@@ -0,0 +1,25 @@
+class nestif
+{
+public static int n=20;
+public static void main(String []args)
+{
+
+	if(n == 20)
+	{
+		if((n%2)==0)
+		{
+			System.out.println("no is even");
+			
+		}
+		else
+		{
+			System.out.println("odd");
+		}
+	}
+	else
+		{
+		System.out.println("enter another number");
+		}
+	
+}
+}
